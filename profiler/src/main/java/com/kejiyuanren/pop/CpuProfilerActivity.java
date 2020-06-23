dca06f77239400493a6a3473a91be7799bd91f4f
xiaojingwa <laoxinqiang@yeah.net>
2020-06-23 10:34:58 +0800

    adjust projuect moudle name

---
+++ profiler/src/main/java/com/kejiyuanren/pop/CpuProfilerActivity.java
@@ -0,0 +1,14 @@
+package com.kejiyuanren.pop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CpuProfilerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpu_profiler);
+    }
+}
