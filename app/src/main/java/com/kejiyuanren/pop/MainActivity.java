8425e68513d0ac522fa83006f3960ab75f946fd9
xiaojingwa <laoxinqiang@yeah.net>
2020-06-23 09:54:26 +0800

    origin project add

---
+++ app/src/main/java/com/kejiyuanren/pop/MainActivity.java
@@ -0,0 +1,25 @@
+package com.kejiyuanren.pop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kejiyuanren.pop.profiler.CpuProfilerActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCpuProfilerTest(View view) {
+        Log.d(TAG, "onCpuProfilerTest() called with: view = [" + view + "]");
+        startActivity(new Intent(this, CpuProfilerActivity.class));
+    }
+}
